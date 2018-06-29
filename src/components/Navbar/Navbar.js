1cf4be2230b8ef8e77a2b5b3944326224d3ff12a
Dan <dpedersen84@gmail.com>
2018-06-28 23:58:40 -0500

    navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = props => {
+
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    
+    <Link className="navbar-brand" to="/">
+        Clicky Game!
+    </Link>
+    
+    
+    
+    
+    </nav>
+
+
+
+
+}
+
+export default Navbar;
