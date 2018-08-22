529a6941e19fdc2232018687869a4c6c394984ed
Dan <dpedersen84@gmail.com>
2018-08-22 11:52:43 -0500

    testing material ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,41 @@
 import React, {Component} from "react";
+import Cardv2 from "../components/Cardv2"
+import Grid from '@material-ui/core/Grid';
+import Button from "../components/Button";
+import GPK from "../data.json";
 
 class About extends Component {
+
+    state = {
+        GPK,
+    }
     
+    onClick = (id) => {
+        console.log(id)
+    }
+
         render() {
             return (
-                <h1>About</h1>
+                <div>
+                    <Grid container spacing={24} style={{padding: 24}}>
+                        <Grid item xs={12} sm={6}>
+                                <Button/>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={24} style={{padding: 24}}>
+                        {this.state.GPK.map(gpk => (
+                            <Grid item xs={12} sm={6} lg={4} xl={3}>
+                                <Cardv2
+                                    key={gpk.id}
+                                    image={gpk.image}
+                                    onClick={()=>{this.onClick(gpk.id)}}
+                                />
+                            </Grid>
+                        ))}
+                        
+                        
+                    </Grid>
+                </div>
             )
         }
 
