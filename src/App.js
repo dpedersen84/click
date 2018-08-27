4275c4a681666766253892a6387f0a48489e92c7
Dan <dpedersen84@gmail.com>
2018-08-26 22:16:19 -0500

    moved navbar and jumbo components into game.js, score updates live

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Jumbotron from "./components/Jumbotron";
 import Footer from "./components/Footer";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Game from "./pages/Game";
 import "./App.css";
 
 const App = () => (
   <Router>
     <div>
-      <Navbar />
-      <Jumbotron backgroundImage="https://i.imgur.com/g2EkSbL.jpg"/>
         <Route exact path="/" component={Game} />
-        <Route exact path="/about" component={About} />
+        {/* <Route exact path="/about" component={About} /> */}
       <Footer />
     </div>
   </Router>
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
 import _ from "underscore";
-import Scoreboard from "../components/Scoreboard";
+// import Scoreboard from "../components/Scoreboard";
 import Card from "../components/Card";
 import cards from "../data.json";
 // import Alert from "../components/Alert";
+import Navbar from "../components/Navbar";
+import Jumbotron from "../components/Jumbotron";
 
 class Game extends Component {
 
@@ -21,24 +23,32 @@ class Game extends Component {
     componentDidMount = () => {
         console.log(this.state)
         this.shuffleCards();
+        console.log("Welcome!")
         console.log("Starting Score:", this.state.score)
         console.log("Top Score:", this.state.topScore)
     }
 
     handleClick = (id) => {
-        console.log(id)
+        // console.log(id)
 
         if (this.state.cardsPicked.includes(id)) {
             this.loseGame();
         } else {
-            this.shuffleCards();
-            this.updateScore();
+            this.correctGuess();
+            // this.shuffleCards();
+            // this.updateScore();
 
             this.state.cardsPicked.push(id)
 
             console.log(this.state.cardsPicked)
         }
     };
+
+    correctGuess = () => {
+        console.log("Correct!")
+        this.updateScore();
+        // this.shuffleCards();
+    }
     
     shuffleCards = () => {
         this.setState({cards: _.shuffle(cards)})
@@ -46,40 +56,52 @@ class Game extends Component {
 
     updateScore = () => {
         let newScore = this.state.score + 1
-        console.log("new score", newScore)
+        console.log("Score", newScore)
         this.setState({ score: newScore })
+
         
         if (newScore > this.state.topScore) {
             console.log("new top score")
         }
-        if (newScore === 3) {
+        if (newScore === 18) {
             this.winGame()
         }
+        this.shuffleCards();
     };
 
     gameRestart = () => {
-        this.setState({ winner: false })
-        this.setState({ gameOver: false })
+        console.log("New Game!")
+
+        // this.setState({ winner: false })
+        // this.setState({ gameOver: false })
+
         this.scoreReset()
         this.cardsPickedReset()
         this.shuffleCards()
-        console.log("New Game!")
+        // this.setState({ score: 0})
+        // console.log("Top Score:", this.state.topScore)
+        // console.log(this.state)
     };
 
     loseGame = () => {
+        alert("Game Over")
         console.log("Game Over")
-        this.setState({ gameOver: true })
-        // this.gameRestart();
+        console.log("===============================")
+        // this.setState({ gameOver: true })
+        this.gameRestart();
     }
 
     winGame = () => {
+        alert("Winner!")
         console.log("Winner!")
-        this.setState({ winner: true })
-        // this.gameRestart();
+        console.log("===============================")
+        // this.setState({ winner: true })
+        this.gameRestart();
     }
 
     scoreReset = () => {
         this.setState({ score: 0})
+        // console.log("Starting Score:", this.state.score)
     };
 
     cardsPickedReset = () => {
@@ -88,50 +110,54 @@ class Game extends Component {
 
     render() {
         return (
-            <div className="container" id="main">
-                {/* <Alert 
-                    type="success"
-                    style={{ opacity: this.state.winner ? 1 : 0, marginBottom: 5 }}
-                >
-                    Winner!
-                </Alert>
+            <div>
+                <Navbar score={this.state.score} topScore={this.state.topScore}/>
+                <Jumbotron backgroundImage="https://i.imgur.com/g2EkSbL.jpg"/>
+                    <div className="container" id="main">
+                        {/* <Alert 
+                            type="success"
+                            style={{ opacity: this.state.winner ? 1 : 0, marginBottom: 5 }}
+                        >
+                            Winner!
+                        </Alert>
 
-                <Alert 
-                    type="warning"
-                    style={{ opacity: this.state.gameOver ? 1 : 0, marginBottom: 5 }}
-                >
-                    Game Over!
-                </Alert> */}
-                {/* <div>
-                    <button 
-                        className="btn btn-danger" 
-                        style={{ opacity: this.state.gameOver || this.state.winner ? 1 : 0, marginBottom: 5 }}
-                        onClick={ () => this.gameRestart() }
-                    >
-                    New Game
-                    </button>
-                </div>
-                <Scoreboard
-                    score= {this.state.score}
-                    topScore = {this.state.topScore}
-                /> */}
+                        <Alert 
+                            type="warning"
+                            style={{ opacity: this.state.gameOver ? 1 : 0, marginBottom: 5 }}
+                        >
+                            Game Over!
+                        </Alert> */}
+                        {/* <div>
+                            <button 
+                                className="btn btn-danger" 
+                                style={{ opacity: this.state.gameOver || this.state.winner ? 1 : 0, marginBottom: 5 }}
+                                onClick={ () => this.gameRestart() }
+                            >
+                            New Game
+                            </button>
+                        </div>
+                        <Scoreboard
+                            score= {this.state.score}
+                            topScore = {this.state.topScore}
+                        /> */}
 
-                {this.state.cards.map(card => (
-                    <Card  
-                        id={card.id}
-                        key={card.id}
-                        image={card.image}
-                        name={card.name}
-                        clicked={card.clicked}
-                        handleClick={() => this.handleClick(card.id)}
-                        // style={{ opacity: this.state.gameOver || this.state.winner ? 0 : 1 }}
-                    />
-                ))}
+                        {this.state.cards.map(card => (
+                            <Card  
+                                id={card.id}
+                                key={card.id}
+                                image={card.image}
+                                name={card.name}
+                                clicked={card.clicked}
+                                handleClick={() => this.handleClick(card.id)}
+                                // style={{ opacity: this.state.gameOver || this.state.winner ? 0 : 1 }}
+                            />
+                        ))}
 
-                
+                        
 
-                
+                        
 
+                    </div>
             </div>
         );
     }
